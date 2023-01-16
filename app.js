eb2777e4872b19a2e537daa4a3e99854a90b4b4b
DoKyeong Oh <dkproh@gmail.com>
2023-01-16 23:23:36 +0900

    chore : i drew a person with canvas in app.js
    - context.arc() is for drawing circle and required x, y, radius, startAngle, endAngle
    - we can use Math.PI for drawing when we use context.arc()

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(200, 200, 50, 200);
-ctx.fillRect(400, 200, 50, 200);
-ctx.lineWidth = 2;
-ctx.fillRect(300, 300, 50, 100);
-ctx.fillRect(200, 200, 200, 20);
-
-ctx.moveTo(200, 200);
-ctx.lineTo(325, 150);
-ctx.lineTo(450, 200);
+ctx.fillRect(230, 200, 10, 100);
+ctx.fillRect(320, 200, 10, 100);
+ctx.fillRect(255, 200, 50, 150);
+ctx.arc(280, 165, 30, 0, Math.PI * 2);
 ctx.fill();
 
+ctx.beginPath();
+ctx.fillStyle = "white";
+ctx.arc(268, 160, 5, Math.PI, Math.PI * 2);
+ctx.arc(292, 160, 5, Math.PI, Math.PI * 2);
+ctx.fill();
 
