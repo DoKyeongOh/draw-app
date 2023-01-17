11e7051f822538d19307df8526e8a09f63e84cdc
DoKyeong Oh <dkproh@gmail.com>
2023-01-17 18:23:11 +0900

    feat : add some color that i am to use

--- app.js
+++ app.js
@@ -4,5 +4,13 @@ canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = 2;
 
+const colors = [
+    "#8c7ae6",
+    "#fbc531",
+    "#e84118",
+    "#192a56",
+    "#2f3640",
+    "#f5f6fa"
+];
 
 
