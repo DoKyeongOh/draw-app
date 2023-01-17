a89f2011295c228149cdd8daa3c134d2f46c8be0
DoKyeong Oh <dkproh@gmail.com>
2023-01-17 18:21:39 +0900

    refactor : i delete some lines that i used
    because i wanna learn other things

--- app.js
+++ app.js
@@ -3,15 +3,5 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(230, 200, 10, 100);
-ctx.fillRect(320, 200, 10, 100);
-ctx.fillRect(255, 200, 50, 150);
-ctx.arc(280, 165, 30, 0, Math.PI * 2);
-ctx.fill();
 
-ctx.beginPath();
-ctx.fillStyle = "white";
-ctx.arc(268, 160, 5, Math.PI, Math.PI * 2);
-ctx.arc(292, 160, 5, Math.PI, Math.PI * 2);
-ctx.fill();
 
