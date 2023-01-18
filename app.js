53e1952d7037438320822bed8a799f514c6b94fc
DoKyeong Oh <dkproh@gmail.com>
2023-01-18 18:01:46 +0900

    feat : add fileInput in app.js
    - i add fileInput that i bring from index.html for image uploading

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const colorOptions = Array.from(document.getElementsByClassName("color-option"))
 const modeBtn = document.getElementById("mode-btn");
 const destroyBtn = document.getElementById("destroy-btn");
 const eraserBtn = document.getElementById("eraser-btn");
+const fileInput = document.getElementById("file");
 
 const CANVAS_WIDTH = 800;
 const CANVAS_HEIGHT = 800;
